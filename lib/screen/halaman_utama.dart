e4d18c603b485f586811e78c1f23b43bbfaca7e9
FerdiMauladi <56813073+FerdiMauladi@users.noreply.github.com>
2022-02-12 11:23:50 +0700

    Update halaman_utama.dart

--- lib/screen/halaman_utama.dart
+++ lib/screen/halaman_utama.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 
 class HalamanUtama extends StatefulWidget {
- HalamanUtama({Key? key}) : super(key: key);
+  HalamanUtama({Key? key}) : super(key: key);
 
   @override
-  State HalamanUtama> createState() =>  HalamanUtamaState();
+  State<HalamanUtama> createState() => _HalamanUtamaState();
 }
 
-class  HalamanUtamaState extends State HalamanUtama> {
+class _HalamanUtamaState extends State<HalamanUtama> {
+  int indexHalaman = 0;
+  List judulHalaman = [
+    "Halaman Beranda",
+    "Halaman Profil",
+    "Halaman Pengaturan",
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Halaman Utama"),
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+        child: Text("Halaman Utama"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home), label: "Halaman Beranda"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), label: "Halaman Profil"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: "Halaman Pengaturan"),
+        ],
+      ),
+    );
   }
 }
