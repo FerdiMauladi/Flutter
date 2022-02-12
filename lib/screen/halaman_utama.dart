6296f75a3c7ca7e6c636dc87738b475a3dc014dd
FerdiMauladi <56813073+FerdiMauladi@users.noreply.github.com>
2022-02-12 11:00:33 +0700

    Create halaman_utama.dart

---
+++ lib/screen/halaman_utama.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HalamanUtama extends StatefulWidget {
+ HalamanUtama({Key? key}) : super(key: key);
+
+  @override
+  State HalamanUtama> createState() =>  HalamanUtamaState();
+}
+
+class  HalamanUtamaState extends State HalamanUtama> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
